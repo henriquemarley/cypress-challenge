3546dc42dc3b34119d04e804e204256351322f97
Henrique <henrique.marley@gmail.com>
2022-02-23 20:22:35 -0300

    Fixing test cases and adding videos do gitignore

--- cypress/integration/signIn.specs.js
+++ cypress/integration/signIn.specs.js
@@ -3,69 +3,79 @@ import SignInPage from '../pages/SignInPage'
 
 describe('Validating Login scenarios', function () {
 
-    it('User should be able to log in with valid credentials', function () {
+    it('Login with valid credentials', function () {
 
         var validCredentials = SignInFactory.validCredentials()
 
         const expectedMessage = 'You logged into a secure area!'
+
         SignInPage.navigate()
         SignInPage.insertCredentials(validCredentials)
         SignInPage.submit()
         SignInPage.flashMessageShouldBe(expectedMessage)
     })
 
-    it('User inputs incorrect password', function () {
+    it('Login with invalid password', function () {
 
         var invalidPassword = SignInFactory.invalidPassword()
 
         const expectedMessage = 'Your password is invalid!'
+
         SignInPage.navigate()
         SignInPage.insertCredentials(invalidPassword)
         SignInPage.submit()
         SignInPage.flashMessageShouldBe(expectedMessage)
     })
 
-    it('User inputs incorrect username', function () {
+    it('Login with invalid username', function () {
 
         var invalidUsername = SignInFactory.invalidUsername()
 
         const expectedMessage = 'Your username is invalid!'
+
         SignInPage.navigate()
         SignInPage.insertCredentials(invalidUsername)
         SignInPage.submit()
         SignInPage.flashMessageShouldBe(expectedMessage)
     })
 
-    it('User inputs invalid credentials in both input fields', function() {
+    it('Login with invalid username and password', function() {
 
         var invalidCredentials = SignInFactory.invalidCredentials()
 
         const expectedMessage = 'Your username is invalid!'
+
         SignInPage.navigate()
         SignInPage.insertCredentials(invalidCredentials)
         SignInPage.submit()
         SignInPage.flashMessageShouldBe(expectedMessage)
     })
 
-    it('User fails to fill both username and password input fields', function () {
+    it('Login with empty credentials', function () {
+
         const expectedMessage = 'Your username is invalid!'
+
         SignInPage.navigate()
         SignInPage.submit()
         SignInPage.flashMessageShouldBe(expectedMessage)
     })
 
     it('User tries to acess Secure Area without logging on the application', function() {
+
         const expectedMessage = 'You must login to view the secure area!'
+
         SignInPage.navigateToSecureArea()
         SignInPage.flashMessageShouldBe(expectedMessage)
     })
 
-    it('Checks if Elemental Selenium link is avaiable', function() {
+    it('Checks if Elemental Selenium link is available', function() {
+
         SignInPage.navigate()
         SignInPage.checkElementalSeleniumLink()
     })
 
     it('User should be able to close warning alert', function() {
+
         SignInPage.navigate()
         SignInPage.submit()
         SignInPage.closeWarningAlert()
@@ -76,6 +86,7 @@ describe('Validating Login scenarios', function () {
         var validCredentials = SignInFactory.validCredentials()
 
         const expectedMessage = 'You logged into a secure area!'
+        
         SignInPage.navigate()
         SignInPage.insertCredentials(validCredentials)
         SignInPage.pressEnterOnPassword()
@@ -85,6 +96,7 @@ describe('Validating Login scenarios', function () {
     it('Checks if password field is masked', function() {
 
         var validCredentials = SignInFactory.validCredentials()
+
         SignInPage.navigate()
         SignInPage.insertCredentials(validCredentials)
         SignInPage.isPasswordMasked()
